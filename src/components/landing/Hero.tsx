8d21a305e3661c166ff6a8357c68dd260f0fe4ad
Rishi kashyap <kashyap1strishi@gmail.com>
2025-10-30 18:47:51 +0530

    landing updates-2

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -45,7 +45,7 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export const Hero = () => {
   const router = useRouter();
